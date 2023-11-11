1f232ebafee5b5a21664e99e3ddfa4dadd99370d
AlexCsms <cosmasalex365@gmail.com>
2023-11-11 19:04:23 +0300

    Added pipeline ci

--- mix.exs
+++ mix.exs
@@ -48,7 +48,12 @@ defmodule PipelineCi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test]}
     ]
   end
 
